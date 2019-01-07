b5793ed346cd50824db7dcdfcf3456bc3340a06f
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-01-07 14:42:51 +0100

    Update articles.vue

--- layouts/articles.vue
+++ layouts/articles.vue
@@ -44,7 +44,7 @@
   padding: 70px 0!important;
 }
 html {
-  font-family: 'Slabo 27px', serif;
+  font-family: 'Domine', serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
